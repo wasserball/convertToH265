b1a50379d845293686a585e5a38ffc0c65801adc
nick <nick@nicks-iMac.(none)>
2017-07-10 20:36:27 +0200

    ealing With Spaces in File Names

---
+++ README.md
@@ -0,0 +1,19 @@
+# h265 converter
+---
+keeps all audio tracks and subs
+
+
+### Usage:
+
+- `cd convertToH265` and run the script with `sh convertToH265.sh` 
+--> converts all files in `in` and stores the results in `out`
+
+
+### requirements / install
+
+1. install [Homebrew](https:/‚/brew.sh/)
+2. install HandBrake-CLI `brew install handbrake`
+
+### To-Do
+
+- [x] Dealing With Spaces in File Names (" " --> "_")
--- convertToH265.sh
+++ convertToH265.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-# this script is to automatically convert a folder of video files to H.265
-# You need to change SRC -- Source folder and DEST -- Destination folder
-
-
-#         $HANDBRAKE_CLI -i $SRC/$FILE -o $DEST/$filename.$DEST_EXT -e x265 -q 20 -E av_aac --custom-anamorphic --keep-display-aspect -O
 # https://handbrake.fr/docs/en/latest/cli/cli-guide.html
 
-SRC=/home/nick/Desktop/convertToH265/in
-DEST=/home/nick/Desktop/convertToH265/out
+SRC=./in/*.*
+DEST=./out
 DEST_EXT=mp4
 HANDBRAKE_CLI=HandBrakeCLI
 
-for FILE in `ls $SRC`
+find $SRC | while read -r FILE
 do
-        filename=$(basename $FILE)
-        extension=${filename##*.}
-        filename=${filename%.*}
 
-        $HANDBRAKE_CLI -i $SRC/$FILE -o $DEST/$filename.$DEST_EXT -e x265 -q 20 --all-audio --all-subtitles
+	# rename file
+	cleanFileName=${FILE// /_}
+	mv "$FILE" "$cleanFileName"
+
+	# build path to output dir
+    filename=$(basename $cleanFileName)
+    extension=${filename##*.}
+    filename=${filename%.*}
+    
+    # start cli
+    $HANDBRAKE_CLI -i $cleanFileName -o $DEST/$filename.$DEST_EXT -e x265 -q 20 --all-audio --all-subtitles
 done
